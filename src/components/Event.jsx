b38b9a4eb08ae9c776648dc2ad8995f49202294b
[Quinton Nickum] <[q.c.nickum@gmail.com]>
2022-03-31 22:01:37 -0500

    Event styling

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,12 +3,17 @@ import React from "react";
 const Event = ({ event }) => {
 
   return(
-    <div className="card" style={{display: 'flex'}}>
-      <h3>{ event.title }</h3>
-      <p>{ event.description }</p>
-      <p>{ event.groupSize }</p>
-      <p>{ event.time }</p>
-      <img src={ event.pictureUrl } alt="Event"/>
+    <div className="card" style={{display: "flex", flexDirection: "row"}}>
+      <div>
+        <img src={ event.pictureUrl } alt="Event" style={{height: "8em", width:"8em"}}/>
+      </div>
+      <div>
+        <h3>{ event.title }</h3>
+        <div>{ event.description }</div>
+        <div>{ event.groupSize }</div>
+        <div>{ event.time }</div>
+      </div>
+      
     </div>
   )
 }
