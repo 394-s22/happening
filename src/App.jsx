9ee22fd63116a258499fb3b3d25f702544332dc4
jacksonemiller <71667842+jacksonemiller@users.noreply.github.com>
2022-04-03 14:40:46 -0500

    +filter component and state to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Event from "./components/Event";
+import Filter from "./components/Filter";
 import { useData } from "./utils/firebase.js"
 
 const App = () => {
-  const [events, loading, error] = useData('/events'); 
+  const [events, loading, error] = useData('/events');
+  const [filters, setFilters] = useState([]);
   
   if (error) return <h1>{error}</h1>;
   if (loading) return <h1>Loading the schedule...</h1>
@@ -12,6 +14,9 @@ const App = () => {
   return (
     <div className='container'>
       <h1>Happening</h1>
+      <div>
+        <Filter filters={ filters } setFilters={ setFilters } />
+      </div>
       <div>
         {events.map((event, index) => <Event event={ event } key={ index }/>)}
       </div>
---
+++ src/components/Filter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Filter = ({ filters, setFilters }) => {
+
+    return (
+        <div/>
+    )
+}
+
+export default Filter;
