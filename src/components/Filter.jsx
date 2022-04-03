5a3650e6b1987e4ec4fc1c831107d8f46b82d893
masonn99 <80999622+masonn99@users.noreply.github.com>
2022-04-03 15:19:22 -0500

    filter options 2

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Filter = ({ filters, setFilters, filterOptions }) => {
+    const [expanded, setExpanded] = useState(false);
 
     return (
         <div style={{ border: "solid 1px #333" }}>
-            <button className="btn btn-primary">Add Filters</button>
+            <div>
+                
+            </div>
+            <button className="btn btn-primary" onClick={() => setExpanded(true)}>Add Filters</button>
+            {
+                (expanded)&& (
+                    filterOptions.map(option => <button>{option}</button>)
+                )
+            }
+
         </div>
     )
 }
