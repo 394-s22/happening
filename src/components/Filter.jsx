11d03be5032276595aa81368891d532fd92361dc
sharatd <sharat.dhananjaya@gmail.com>
2022-04-03 16:13:35 -0500

    adding and removing filters

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 const Filter = ({ filters, setFilters, filterOptions }) => {
     const [expanded, setExpanded] = useState(false);
 
+    const addFilter = (f) => setFilters([...filters, f])
+    const removeFilter = (f) => setFilters(filters.filter(selected => selected !== f))
     return (
         <div style={{ border: "solid 1px #333" }}>
             <div>
-                
+                <button className="btn btn-primary" onClick={() => setExpanded(!expanded)}>Add Filters</button>
+                {filters.map(f => <button onClick={() => removeFilter(f)}>{f}</button>)}    
             </div>
-            <button className="btn btn-primary" onClick={() => setExpanded(true)}>Add Filters</button>
+            
             {
                 (expanded)&& (
-                    filterOptions.map(option => <button>{option}</button>)
+                    filterOptions.filter(option => !filters.includes(option)).map(option => <button onClick={() => addFilter(option)} >{option}</button>)
                 )
             }
 
