52b430246aa40f54183db20d76b92806e5e4e2ff
RutujaPK <90537559+RutujaPK@users.noreply.github.com>
2022-04-17 21:27:51 -0500

    Getting events

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,38 +1,36 @@
-
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
 
-const Event = require('./schema/Event.js');
+const Event = require("./schema/Event.js");
 
 // Define all API routes
 
-router.route('/')
-  .get((req, res) => {
-    res.status(200).send('<h1>Happening API</h1>');
-  });
+router.route("/").get((req, res) => {
+  res.status(200).send("<h1>Happening API</h1>");
+});
 
-router.route('/events')
-  .get((req, res) => {
-    res.status(200).send('GET ALL EVENTS NOW');
+router.route("/events").get((req, res) => {
+  Event.find({}).then((data) => {
+    res.status(200).send(data);
   });
+});
 
-router.route('/events/new')
-  .post((req, res) => {
-    res.status(200).send('MAKE NEW EVENT');
-  });
+router.route("/events/new").post((req, res) => {
+  res.status(200).send("MAKE NEW EVENT");
+});
 
-router.route('/events/:eid/rsvp')
+router
+  .route("/events/:eid/rsvp")
   .post((req, res) => {
     res.status(200).send(`NEW RSVP TO EVENT ${req.params.eid}`);
   })
   .delete((req, res) => {
-    res.status(200).send('CANCEL RSVP TO EVENT');
+    res.status(200).send("CANCEL RSVP TO EVENT");
   });
 
-router.route('/user/:uid/rsvp')
-  .get((req, res) => {
-    res.status(200).send(`RSVP events for ${req.params.uid}`);
-  });
+router.route("/user/:uid/rsvp").get((req, res) => {
+  res.status(200).send(`RSVP events for ${req.params.uid}`);
+});
 
 module.exports = router;
