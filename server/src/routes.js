f33ec93b8170dfc6b9e8599c36ec3cf1062539cb
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2022-04-17 22:34:53 -0500

    Start adding Users

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const Event = require("./schema/Event.js");
+const User = require("./schema/User.js");
 
 // Define all API routes
 
@@ -23,7 +24,32 @@ router.route("/events/new").post((req, res) => {
 router
   .route("/events/:eid/rsvp")
   .post((req, res) => {
-    res.status(200).send(`NEW RSVP TO EVENT ${req.params.eid}`);
+    const { email } = req.body;
+    const eid = req.params.eid;
+    
+    User.findOne({ email })
+      .then((user) => {
+        if (!user) {
+          // Create user entry
+          const newUser = {
+            email,
+            rsvp: [eid],
+          };
+          User.create(newUser).save()
+            .then((user) => {
+              res.status(204).send();
+            });
+          return;
+        } 
+
+        const newRsvp = user.rsvp.includes(eid) ? user.rsvp : [eid, ...user.rsvp];
+        User.findByIdAndUpdate(user._id, {rsvp: newRsvp})
+          .then(data => {
+            res.status(204).send();
+            return;
+          });
+
+      });
   })
   .delete((req, res) => {
     res.status(200).send("CANCEL RSVP TO EVENT");
