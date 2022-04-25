b13a06ef92acddc96f8176af6f9a102b01b25617
RutujaKajave <rutuja.kajave.rk@gmail.com>
2022-04-24 21:08:12 -0500

    Bugs solved. Hurray

--- server/src/routes.js
+++ server/src/routes.js
@@ -8,24 +8,22 @@ const User = require("./schema/User.js");
 
 // Define all API routes
 
-router.route("/")
-  .get((req, res) => {
-    res.status(200).send("<h1>Happening API</h1>");
-  });
+router.route("/").get((req, res) => {
+  res.status(200).send("<h1>Happening API</h1>");
+});
 
-router.route("/events")
-  .get((req, res) => {
-    Event.find({}).then((data) => {
-      res.status(200).send({ events: data });
-    });
+router.route("/events").get((req, res) => {
+  Event.find({}).then((data) => {
+    res.status(200).send({ events: data });
   });
+});
 
-router.route("/events/new")
-  .post((req, res) => {
-    res.status(200).send("MAKE NEW EVENT");
-  });
+router.route("/events/new").post((req, res) => {
+  res.status(200).send("MAKE NEW EVENT");
+});
 
-router.route("/events/:eid/rsvp/:uid")
+router
+  .route("/events/:eid/rsvp/:uid")
   .post(async (req, res) => {
     const { eid, uid } = req.params;
 
@@ -33,18 +31,18 @@ router.route("/events/:eid/rsvp/:uid")
     const user = await User.findById(uid);
 
     if (!event) {
-      res.status(404).send({ message: `Event with id '${eid}' does not exist`});
+      res
+        .status(404)
+        .send({ message: `Event with id '${eid}' does not exist` });
       return;
     }
 
     if (!user) {
-      res.status(404).send({ message: `User with id '${uid}' does not exist`});
+      res.status(404).send({ message: `User with id '${uid}' does not exist` });
       return;
     }
 
-    
     if (!user.rsvp.includes(eid)) {
-
       await User.findByIdAndUpdate(uid, { rsvp: [eid, ...user.rsvp] });
     }
 
@@ -62,58 +60,60 @@ router.route("/events/:eid/rsvp/:uid")
     const user = await User.findById(uid);
 
     if (!event) {
-      res.status(404).send({ message: `Event with id '${eid}' does not exist`});
+      res
+        .status(404)
+        .send({ message: `Event with id '${eid}' does not exist` });
       return;
     }
 
     if (!user) {
-      res.status(404).send({ message: `User with id '${uid}' does not exist`});
+      res.status(404).send({ message: `User with id '${uid}' does not exist` });
       return;
     }
 
-    
     if (user.rsvp.includes(eid)) {
-
-      await User.findByIdAndUpdate(uid, { rsvp: user.rsvp.filter((rsvp) => rsvp !== eid)});
+      await User.findByIdAndUpdate(uid, {
+        rsvp: user.rsvp.filter((rsvp) => rsvp.toString() !== eid),
+      });
     }
 
     if (event.rsvp.includes(uid)) {
-      await Event.findByIdAndUpdate(eid, { rsvp: event.rsvp.filter((rsvp) => rsvp !== uid)});
+      await Event.findByIdAndUpdate(eid, {
+        rsvp: event.rsvp.filter((rsvp) => rsvp.toString() !== uid),
+      });
     }
 
     res.status(204).send();
     return;
-  })
-  
-router.route("/user/:uid/rsvp")
-  .get((req, res) => {
-    User.findById(req.params.uid).then((user) => {
-      Event.find({ _id: { $in: user.rsvp } }).then((data) => {
-        res.status(200).send({ rsvp: data });
-      });
-    });
   });
 
-router.route("/user/login")
-  .post((req, res) => {
-    console.log(req.body);
-    const { email } = req.body;
-    User.findOne({ email }).then((user) => {
-      if (user) {
-        res.status(200).send({ user });
-        return;
-      }
-      // Create user entry
-      const newUser = {
-        email,
-        rsvp: [],
-      };
-      User.create(newUser)
-        .save()
-        .then((user) => {
-          res.status(200).send({ user });
-        });
+router.route("/user/:uid/rsvp").get((req, res) => {
+  User.findById(req.params.uid).then((user) => {
+    Event.find({ _id: { $in: user.rsvp } }).then((data) => {
+      res.status(200).send({ rsvp: data });
     });
   });
+});
+
+router.route("/user/login").post((req, res) => {
+  console.log(req.body);
+  const { email } = req.body;
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      res.status(200).send({ user });
+      return;
+    }
+    // Create user entry
+    const newUser = {
+      email,
+      rsvp: [],
+    };
+    User.create(newUser)
+      .save()
+      .then((user) => {
+        res.status(200).send({ user });
+      });
+  });
+});
 
 module.exports = router;
