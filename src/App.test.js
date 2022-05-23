8ec77d8729c066ba53e95114002d1b0e818f9d88
[Quinton Nickum] <q.c.nickum@gmail.com>
2022-05-23 10:50:33 -0500

    not working :(

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import { useData, useUserState }from './utils/firebase';
+import '@testing-library/jest-dom';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -6,3 +8,25 @@ test('renders learn react link', () => {
   const headerElement = screen.getByText(/Happened/i);
   expect(headerElement).toBeInTheDocument();
 });
+
+jest.mock('./utils/firebase');
+
+test('authenticated user sees something different', () => {
+  const mockUser = {
+    email: 'test@u.northwestern.edu'
+  };
+
+  useData.mockReturnValue([{events: []}, false, null]);
+  useUserState.mockReturnValue(mockUser);
+  render(<App />)
+  const signOutButton = screen.getByText(/Sign Out/i)
+  expect(signOutButton).toBeInTheDocument();
+})
+
+test('non authenticated user', () => {
+  useData.mockReturnValue([{events: []}, false, null]);
+  useUserState.mockReturnValue(null);
+  render(<App />)
+  const signOutButton = screen.getByText(/Sign In/i)
+  expect(signOutButton).toBeInTheDocument();
+})
