1ea9a0b7ef892f298aa6bc9bcd87a82e9cbdd494
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2022-05-23 10:33:08 -0500

    Fixed initial test code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = createRoot(document.querySelector('#root'));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
